b516280c4b0e21cff563f53aedc7c72fc176d076
Florent Lagrede <florent.lagrede@macbook-pro-de-florent.home>
2019-11-15 12:00:47 +0100

    feat: add latest articles on home page

---
+++ content/latest-articles.js
@@ -0,0 +1,29 @@
+const latestArticles = [
+  {
+    title:
+      "[EN] Theme UI, the missing piece for building dynamic design system in React",
+    link: "https://www.flagrede.com/blog/theme-ui/",
+    date: "2019-10-25",
+    description:
+      "Theme UI is a react library to style your application while keeping your design rules in check. It offers the power of CSS-in-JS plus…"
+  },
+  {
+    title:
+      "[FR] Analyser les performances de rendu de son interface avec du profiling",
+    link:
+      "https://blog.octo.com/analysez-les-performances-de-rendu-de-son-interface-avec-du-profiling/",
+    date: "2019-10-17",
+    description:
+      "Généralement quand on parle de performances web, on fait référence au premier chargement de la page. Cet article va se concentrer sur un autre type de performance, à savoir la performance de rendu..."
+  },
+  {
+    title: "[FR] Introduire une culture de la performance web sur son projet",
+    link:
+      "https://blog.octo.com/introduire-une-culture-de-la-performance-web-sur-son-projet",
+    date: "2018-11-19",
+    description:
+      "Les performances web ne constituent pas toujours une priorité dans un projet, particulièrement dans les premières phases de développement..."
+  }
+];
+
+export default latestArticles;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 /** @jsx jsx */
-import { jsx } from "theme-ui";
+import { Styled, jsx } from "theme-ui";
 
+import { Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import patternBg from "../images/taieri.svg";
+import latestArticles from "../../content/latest-articles";
 
 function IndexPage() {
   return (
@@ -20,22 +22,73 @@ function IndexPage() {
           width: "100%"
         }}
       >
-        <h2
+        <section
           sx={{
             display: "flex",
-            fontWeight: "bold",
-            color: "text",
-            fontWeight: "bold",
+            flexDirection: "column",
             alignItems: "center",
-            my: 8,
-            p: 10,
-            fontSize: "3rem",
-            color: "gray",
-            width: ["100%", "100%", "50%"]
+            textAlign: "center",
+            width: ["100%", "100%", "50%"],
+            py: 4
           }}
         >
-          Hi, I'm Florent. A freelance front-end developper based in Paris.
-        </h2>
+          <h2
+            sx={{
+              display: "flex",
+              fontWeight: "bold",
+              color: "text",
+              fontWeight: "bold",
+              alignItems: "center",
+              my: 2,
+              p: 10,
+              fontSize: "3rem",
+              color: "text",
+              width: "100%"
+            }}
+          >
+            Hi, I'm Florent. A freelance front-end developper based in Paris.
+          </h2>
+          <ul sx={{ mt: 4, width: ["90%", "90%", "70%"] }}>
+            <h2
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                fontWeight: "bold",
+                color: "text",
+                fontWeight: "bold",
+                alignItems: "center",
+                my: 2,
+                p: 10,
+                fontSize: 4,
+                color: "#4a5568",
+                width: "100%"
+              }}
+            >
+              Latest articles:
+            </h2>
+            {latestArticles.slice(0, 3).map(article => (
+              <li>
+                <Styled.h2
+                  sx={{
+                    mb: 1,
+                    fontSize: 2
+                  }}
+                >
+                  <Styled.a
+                    sx={{
+                      textDecoration: `none`
+                    }}
+                    href={article.link}
+                  >
+                    {article.title}
+                  </Styled.a>
+                </Styled.h2>
+                <small>{article.date}</small>
+                <Styled.p sx={{ fontSize: 1 }}>{article.description}</Styled.p>
+              </li>
+            ))}
+          </ul>
+        </section>
         <div
           sx={{ width: ["100%", "100%", "50%"] }}
           style={{ backgroundImage: `url(${patternBg})` }}
